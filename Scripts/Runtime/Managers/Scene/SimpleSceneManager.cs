9b7d5c203fa6872d84281b1f4093f990dcf29f72
Johnny Mast <mastjohnny@gmail.com>
2021-04-11 14:06:03 +0200

    Added Scene manager and default scenes.

---
+++ Scripts/Runtime/Managers/Scene/SimpleSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSceneManager : MonoBehaviour
+{
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
